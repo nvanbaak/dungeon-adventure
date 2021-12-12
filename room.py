5b36f2756646b186f7743a152bbace734fee49c5
Fikadulem <fikadulem@gmail.com>
2021-12-12 10:25:20 -0800

    room development in progress

---
+++ room.py
@@ -0,0 +1,31 @@
+import random
+class room:
+    def __init__(self):
+        self.__health_p = True
+        self.__vision_p = True
+        self.__impassable = False
+        self.__pit = False
+        self.__entrance = True
+        self.__exit = True
+        self.__doors = {
+            "n": None,
+            "w": None,
+            "e": None,
+            "s": None
+        }
+        self.__has_player = True
+        self.__pillar = ["A", "E", "I", "P"]
+    # builds 2D Graphical representation of the room
+    def __str__(self):
+        return f''
+    def set_health(self, add_potion):
+        self.__health_p = add_potion
+
+    def can_enter(self):
+        return not self.__impassable and not self.__visited
+
+    def is_exit(self):
+        return self.__exit
+
+    def set_visited(self, visited):
+        self.__visited = visited
