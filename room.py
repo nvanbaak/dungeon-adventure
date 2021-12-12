85e321441918295c12930769ae6ea46d951dada6
Fikadulem <fikadulem@gmail.com>
2021-12-12 14:32:00 -0800

    Still working on it

--- room.py
+++ room.py
@@ -1,5 +1,7 @@
 import random
-class room:
+
+
+class Room:
     def __init__(self):
         self.__health_p = True
         self.__vision_p = True
@@ -14,10 +16,31 @@ class room:
             "s": None
         }
         self.__has_player = True
-        self.__pillar = ["A", "E", "I", "P"]
+        self.__pillar = False
     # builds 2D Graphical representation of the room
     def __str__(self):
         return f''
+
+    def link(self, room, dir):
+        make_link = True
+        if dir == "n":
+            if room.get_s() is False:
+                make_link = False
+        if dir == "w":
+            if room.get_e() is False:
+                make_link = False
+        if dir == "e":
+            if room.get_w() is False:
+                make_link = False
+        if dir == "s":
+            if room.get_n() is False:
+                make_link = False
+
+        if make_link:
+            self.compass_dict[dir] = room
+        else:
+            self.compass_dict[dir] = False
+
     def set_health(self, add_potion):
         self.__health_p = add_potion
 
@@ -29,3 +52,14 @@ class room:
 
     def set_visited(self, visited):
         self.__visited = visited
+
+    def set_has_player(self, player):
+        self.__has_player = player
+
+    def set_pillar(self, pillar):
+        pillars = ["A", "E", "I", "P"]
+        self.__pillar = pillar
+
+
+
+
